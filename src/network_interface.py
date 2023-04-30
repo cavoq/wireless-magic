0f0b17a2c1fbc985e93317773f6fae7cf15d6da2
David Stromberger <DavidStromberger99@gmx.de>
2023-04-30 17:39:20 +0200

    Update network interface

--- src/network_interface.py
+++ src/network_interface.py
@@ -1,11 +1,50 @@
 import os
+import subprocess
 from typing import List
 
 
-def get_wifi_interfaces() -> List[str]:
+class NetworkInterface:
+    def __init__(self, interface: str):
+        self.interface: str = interface
+        self.mac_address: str = self.get_mac_address()
+        self.mode = self.get_mode()
+
+    def update(self) -> None:
+        self.mac_address = self.get_mac_address()
+        self.mode = self.get_mode()
+    
+    def get_mac_address(self) -> str:
+        mac_address_file: str = f"/sys/class/net/{self.interface}/address"
+        with open(mac_address_file, "r") as f:
+            mac_address: str = f.read().strip()
+        return mac_address
+
+    def get_mode(self) -> str:
+        mode_file: str = f"/sys/class/net/{self.interface}/type"
+        with open(mode_file, "r") as f:
+            mode: str = f.read().strip()
+        if mode == "1":
+            return "Managed"
+        if mode == "803":
+            return "Monitor"
+
+    def set_mode(self, mode: str) -> bool:
+        if mode != ("managed" or "monitor"):
+            return False
+        cmd = f"sudo iwconfig {self.interface} mode {mode}"
+        result = subprocess.run(cmd.split(), capture_output=True)
+        if result.returncode != 0:
+            return False
+        return True
+            
+    def to_string(self) -> str:
+        return f"Interface: {self.interface}\nMAC Address: {self.mac_address}\nMode: {self.mode}"
+
+
+def get_wifi_interfaces() -> List[NetworkInterface]:
     wifi_interfaces = []
     net_dir: str = "/sys/class/net"
     for interface in os.listdir(net_dir):
         if os.path.exists(os.path.join(net_dir, interface, "wireless")):
-            wifi_interfaces.append(interface)
+            wifi_interfaces.append(NetworkInterface(interface))
     return wifi_interfaces
--- twiner.py
+++ twiner.py
@@ -1,6 +1,6 @@
 import cmd
 import colorama
-from src.network_interface import get_wifi_interfaces
+from src.network_interface import *
 
 
 colorama.init()
@@ -17,10 +17,11 @@ __________       _____________     __________________
    )_(   (_______)\_______/|/    )_)(_______/|/   \__/
    
    
-[+] Welcome to Twiner, a evil-twin attack tool.
+[+] Welcome to Twiner, a evil-twin attack bot.
 [clear] Clear the screen.
 [quit] Quit the program.
 [ifaces] Get list of available network interfaces.
+[iface <interface>] Set the network interface to use.
 {colorama.Style.RESET_ALL}"""
     prompt = f"{colorama.Fore.BLUE}> {colorama.Style.RESET_ALL}"
 
@@ -32,8 +33,15 @@ __________       _____________     __________________
         return True
     
     def do_ifaces(self, arg=None):
-        print(get_wifi_interfaces())
+        network_interfaces: List[NetworkInterface] = get_wifi_interfaces()
+        for interface in network_interfaces:
+            print(f"{colorama.Fore.GREEN}{interface.to_string()}", end="\n\n")
 
-
+    def do_iface(self, arg=None):
+        if arg is None:
+            print(f"{colorama.Fore.RED}Please specify an interface.{colorama.Style.RESET_ALL}")
+            return
+        
+   
 if __name__ == '__main__':
     TwinerCmd().cmdloop()
