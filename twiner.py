8bb50e0e9bd96bad6b04ec6107684786a9eef722
David Stromberger <DavidStromberger99@gmx.de>
2023-04-30 16:36:22 +0200

    Basic cli

--- twiner.py
+++ twiner.py
@@ -1,5 +1,39 @@
+import cmd
+import colorama
+from src.network_interface import get_wifi_interfaces
 
 
+colorama.init()
 
-if __name__ == "__main__":
-    print("Hello, World!")
+class TwinerCmd(cmd.Cmd):
+    intro = f"""{colorama.Fore.GREEN}
+__________       _____________     __________________
+\__   __/|\     /|\__   __/( (    /|(  ____ \(  ____ )
+   ) (   | )   ( |   ) (   |  \  ( || (    \/| (    )|
+   | |   | | _ | |   | |   |   \ | || (__    | (____)|
+   | |   | |( )| |   | |   | (\ \) ||  __)   |     __)
+   | |   | || || |   | |   | | \   || (      | (\ (   
+   | |   | () () |___) (___| )  \  || (____/\| ) \ \__
+   )_(   (_______)\_______/|/    )_)(_______/|/   \__/
+   
+   
+[+] Welcome to Twiner, a evil-twin attack tool.
+[clear] Clear the screen.
+[quit] Quit the program.
+[ifaces] Get list of available network interfaces.
+{colorama.Style.RESET_ALL}"""
+    prompt = f"{colorama.Fore.BLUE}> {colorama.Style.RESET_ALL}"
+
+    def do_clear(self, arg=None):
+        print(self.intro)
+    
+    def do_quit(self, arg=None):
+        print(f"{colorama.Fore.RED}Quitting...{colorama.Style.RESET_ALL}")
+        return True
+    
+    def do_ifaces(self, arg=None):
+        print(get_wifi_interfaces())
+
+
+if __name__ == '__main__':
+    TwinerCmd().cmdloop()
